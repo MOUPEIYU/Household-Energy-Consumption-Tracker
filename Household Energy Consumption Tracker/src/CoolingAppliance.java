04978adb68be75f93e0b998415892a194f008605
wanshiyu626 <wanshiyu155@gmail.com>
2026-06-24 17:06:05 +0800

    Update CoolingAppliance.java

--- Household Energy Consumption Tracker/src/CoolingAppliance.java
+++ Household Energy Consumption Tracker/src/CoolingAppliance.java
@@ -1,70 +1,35 @@
-/**
- * Parent class for all household electrical appliances, implement encapsulation
- */
-public class Appliance {
-    // Private encapsulated attributes
-    private String name;
-    private double powerRating; // Rated power in kilowatts (kW)
-    private double usageDuration; // Daily usage time in hours (h)
+public class CoolingAppliance extends Appliance {
+    // Unique attribute: user set cooling temperature
+    private double temperatureSetting;
 
-    // Parameterized constructor
-    public Appliance(String name, double powerRating, double usageDuration) {
-        this.name = name;
-        // Data validation: power cannot be negative
-        if (powerRating < 0) {
-            this.powerRating = 0;
-        } else {
-            this.powerRating = powerRating;
-        }
-        // Data validation: usage time cannot be negative
-        if (usageDuration < 0) {
-            this.usageDuration = 0;
-        } else {
-            this.usageDuration = usageDuration;
-        }
+    // Parameterized constructor for cooling appliances
+    public CoolingAppliance(String name, double powerRating, double usageDuration, double temperatureSetting) {
+        super(name, powerRating, usageDuration);
+        this.temperatureSetting = temperatureSetting;
     }
 
-    // Getter method for appliance name
-    public String getName() {
-        return name;
+    // Getter for temperature setting value
+    public double getTemperatureSetting() {
+        return temperatureSetting;
     }
 
-    // Getter method for power rating
-    public double getPowerRating() {
-        return powerRating;
+    // Setter to update cooling temperature
+    public void setTemperatureSetting(double temp) {
+        this.temperatureSetting = temp;
     }
 
-    // Getter method for usage duration
-    public double getUsageDuration() {
-        return usageDuration;
-    }
-
-    // Setter for appliance name
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // Setter with validation for power rating
-    public void setPowerRating(double powerRating) {
-        if (powerRating >= 0) {
-            this.powerRating = powerRating;
-        } else {
-            System.out.println("Power rating cannot be negative, modification failed");
-        }
-    }
-
-    // Setter with validation for usage duration
-    public void setUsageDuration(double usageDuration) {
-        if (usageDuration >= 0) {
-            this.usageDuration = usageDuration;
-        } else {
-            System.out.println("Usage duration cannot be negative, modification failed");
-        }
-    }
-
-    // Base method to calculate power consumption, to be overridden by subclasses for polymorphism
+    /**
+     * Override energy consumption calculation to implement polymorphism
+     * If temperature is below 22°C, cooling load increases, extra 20% power consumption
+     */
+    @Override
     public double calculateEnergyConsumption() {
-        // Formula: Power × Usage Time = Total energy (kWh)
-        return powerRating * usageDuration;
+        double baseConsume = super.calculateEnergyConsumption();
+        // Low temperature setting causes higher energy cost
+        if (temperatureSetting < 22) {
+            return baseConsume * 1.2;
+        } else {
+            return baseConsume;
+        }
     }
 }
